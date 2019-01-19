13a41e7259e388a97e0bb17bc68475406c2d0927
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-19 16:44:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'keylogger',
+    'author': 'Adisakshya Chauhan',
+    'url': 'https://github.com/adisakshya/keylogger',
+    'download_url': 'https://github.com/adisakshya/keylogger',
+    'author_email': 'adisakshya98@gmail.com',
+    'version': '1.0',
+    'install_requires': ['pynput'],
+    'packages': ['pynput'],
+    'scripts': ['bin/keylogger-windows.py'],
+    'name': 'keylogger'
+    }
+
+setup(**config)
